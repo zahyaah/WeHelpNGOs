cc6d60603468572b92c3c77be9b95674ffc86f06
K Zayd Ahmed <itszayd2003@gmail.com>
2023-01-10 14:06:00 +0530

    Removed all the files

--- home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.HomePage.as_view(), name='home_index')
-]
--- home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, UpdateView
-from datetime import datetime
-import time
-
-
-class HomePage(TemplateView):
-    template_name = 'home/index.html'
-    extra_context = {'checkDateTime': datetime.now().strftime('%B %d, %Y'), 'checkTime': datetime.now().strftime("%H:%M:%S")}
